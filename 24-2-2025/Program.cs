57579319b2d1ba8d62e98e3baf9ddef1f8388d5e
hiimquang2k <hnq171200@gmail.com>
2025-02-24 20:36:56 +0700

    Add files via upload

---
+++ 24-2-2025/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24_2_2025
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            DS_NhanVat dS_NhanVat = new DS_NhanVat();
+            dS_NhanVat.Nhap();
+            Console.WriteLine(dS_NhanVat.KiemTra1() ? "Có va chạm" : "Không va chạm");
+            dS_NhanVat.KiemTra2();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 24-2-2025/Ryu.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24_2_2025
+{
+    internal class Ryu:NhanVat
+    {
+         public Ryu()
+            {
+            this.Ten = "Ryu";
+            this.ChieuDai = 17;
+            this.ChieuCao = 32;
+            }
+    }
+}
